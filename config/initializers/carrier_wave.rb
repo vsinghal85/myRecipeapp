aa6c9951a1c8c683dbf4efbe25434043a584e970
Vaibhav Singhal <vsinghal85@gmail.com>
2017-10-24 06:57:21 -0400

    test4

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
             :provider => 'AWS',
             :aws_access_key_id => ENV['S3_ACCESS_KEY'],
             :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-            :endpoint               => "https://s3.amazonaws.com"
+            :endpoint               => "https://s3.amazonaws.com",
             :region                 => "us-east-2"
 
         }
